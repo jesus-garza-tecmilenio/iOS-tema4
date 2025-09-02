019cd66eee95fdcf18f03177f49df29fb4a4bc2e
jesusgarza <jesus.garza.gro@hotmail.com>
2025-09-02 12:31:04 -0600

    Initial Commit

---
+++ Tema4Swift/Tema4SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tema4SwiftApp.swift
+//  Tema4Swift
+//
+//  Created by JESUS GARZA on 02/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Tema4SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
